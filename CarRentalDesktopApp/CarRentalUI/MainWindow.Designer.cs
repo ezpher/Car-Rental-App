84fbc305fad8bfa25bb2863d8d41cff09aaad647
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-09-07 15:22:18 +0800

    Add Basic Dashboard, and checkout car/check-in car functionalities

---
+++ CarRentalDesktopApp/CarRentalUI/MainWindow.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CarRentalDesktopApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainWindowTitle = new System.Windows.Forms.Label();
+            this.panelMainWindowTitle = new System.Windows.Forms.Panel();
+            this.menuStripMainWindow = new System.Windows.Forms.MenuStrip();
+            this.manageRentalsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkoutCarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkinCarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMainWindow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainWindowTitle
+            // 
+            this.mainWindowTitle.AutoSize = true;
+            this.mainWindowTitle.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.mainWindowTitle.Font = new System.Drawing.Font("Matura MT Script Capitals", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainWindowTitle.Location = new System.Drawing.Point(398, 9);
+            this.mainWindowTitle.Name = "mainWindowTitle";
+            this.mainWindowTitle.Size = new System.Drawing.Size(569, 47);
+            this.mainWindowTitle.TabIndex = 1;
+            this.mainWindowTitle.Text = "Rental Car Management System";
+            // 
+            // panelMainWindowTitle
+            // 
+            this.panelMainWindowTitle.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panelMainWindowTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMainWindowTitle.Location = new System.Drawing.Point(0, 24);
+            this.panelMainWindowTitle.Name = "panelMainWindowTitle";
+            this.panelMainWindowTitle.Size = new System.Drawing.Size(979, 48);
+            this.panelMainWindowTitle.TabIndex = 3;
+            // 
+            // menuStripMainWindow
+            // 
+            this.menuStripMainWindow.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.menuStripMainWindow.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageRentalsToolStripMenuItem});
+            this.menuStripMainWindow.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMainWindow.Name = "menuStripMainWindow";
+            this.menuStripMainWindow.Size = new System.Drawing.Size(979, 24);
+            this.menuStripMainWindow.TabIndex = 4;
+            this.menuStripMainWindow.Text = "menuStrip1";
+            // 
+            // manageRentalsToolStripMenuItem
+            // 
+            this.manageRentalsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkoutCarToolStripMenuItem,
+            this.checkinCarToolStripMenuItem});
+            this.manageRentalsToolStripMenuItem.Name = "manageRentalsToolStripMenuItem";
+            this.manageRentalsToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.manageRentalsToolStripMenuItem.Text = "Manage Rentals";
+            // 
+            // checkoutCarToolStripMenuItem
+            // 
+            this.checkoutCarToolStripMenuItem.Name = "checkoutCarToolStripMenuItem";
+            this.checkoutCarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkoutCarToolStripMenuItem.Text = "Checkout Car";
+            this.checkoutCarToolStripMenuItem.Click += new System.EventHandler(this.checkoutCarToolStripMenuItem_Click);
+            // 
+            // checkinCarToolStripMenuItem
+            // 
+            this.checkinCarToolStripMenuItem.Name = "checkinCarToolStripMenuItem";
+            this.checkinCarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkinCarToolStripMenuItem.Text = "Check-in Car";
+            this.checkinCarToolStripMenuItem.Click += new System.EventHandler(this.checkinCarToolStripMenuItem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 450);
+            this.Controls.Add(this.mainWindowTitle);
+            this.Controls.Add(this.panelMainWindowTitle);
+            this.Controls.Add(this.menuStripMainWindow);
+            this.IsMdiContainer = true;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Car Rental Management System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStripMainWindow.ResumeLayout(false);
+            this.menuStripMainWindow.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label mainWindowTitle;
+        private System.Windows.Forms.Panel panelMainWindowTitle;
+        private System.Windows.Forms.MenuStrip menuStripMainWindow;
+        private System.Windows.Forms.ToolStripMenuItem manageRentalsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkoutCarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkinCarToolStripMenuItem;
+    }
+}
+
